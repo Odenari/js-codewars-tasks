e6edf5a1b276a00d427c131c1069efcd5ae5cb4a
Odenari <odenari44@gmail.com>
2023-04-22 16:10:05 +0200

    timer function added

--- firNutriotion/js/script.js
+++ firNutriotion/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+	//Tabs
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContentList = document.querySelectorAll('.tabcontent'),
 		tabsParent = document.querySelector('.tabheader__items');
@@ -34,4 +35,57 @@ document.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
+
+	//Timer code ahead
+	const deadline = '2023-04-30';
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+			mins = Math.floor((t / 1000 / 60) % 60),
+			seconds = Math.floor(t / 1000) % 60;
+		return {
+			total: t,
+			days,
+			hours,
+			mins,
+			seconds
+		};
+	}
+
+
+	function getZero(value) {
+		if (value >= 0 && value < 10) {
+			return `0${value}`;
+		} else {
+			return value;
+		}
+	}
+
+
+	function setClock(selector, endtime) {
+
+		const timer = document.querySelector(selector),
+			days = timer.querySelector('#days'),
+			hours = timer.querySelector('#hours'),
+			mins = timer.querySelector('#mins'),
+			seconds = timer.querySelector('#seconds'),
+			interval = setInterval(updateClock, 1000); //actually legit way to keep that timer running
+
+		updateClock();
+
+		function updateClock() {
+			const timerValues = getTimeRemaining(endtime);
+			days.innerHTML = getZero(timerValues.days);
+			hours.innerHTML = getZero(timerValues.hours);
+			mins.innerHTML = getZero(timerValues.mins);
+			seconds.innerHTML = getZero(timerValues.seconds);
+
+			if (timerValues.total <= 0) {
+				clearInterval(interval);
+			}
+		}
+
+	}
+	setClock('.timer', deadline);
 });
