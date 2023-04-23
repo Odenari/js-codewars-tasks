4bdc476bb7cf47bd7f1ee78fffd12c360290a9a9
Odenari <odenari44@gmail.com>
2023-04-23 20:29:26 +0200

    modal windows functional

--- firNutriotion/js/script.js
+++ firNutriotion/js/script.js
@@ -1,4 +1,7 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', () => {
+
 	//Tabs
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContentList = document.querySelectorAll('.tabcontent'),
@@ -36,14 +39,20 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	});
 
-	//Timer code ahead
-	const deadline = '2023-04-30';
+	//Timer
+	const deadline = '2023-04-27';
 	function getTimeRemaining(endtime) {
-		const t = Date.parse(endtime) - Date.parse(new Date()),
-			days = Math.floor(t / (1000 * 60 * 60 * 24)),
-			hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-			mins = Math.floor((t / 1000 / 60) % 60),
+		let days, hours, mins, seconds;
+		const t = Date.parse(endtime) - Date.parse(new Date());
+
+		if (t <= 0) {
+			days = hours = mins = seconds = 0;
+		} else {
+			days = Math.floor(t / (1000 * 60 * 60 * 24));
+			hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+			mins = Math.floor((t / 1000 / 60) % 60);
 			seconds = Math.floor(t / 1000) % 60;
+		}
 		return {
 			total: t,
 			days,
@@ -52,7 +61,6 @@ document.addEventListener('DOMContentLoaded', () => {
 			seconds
 		};
 	}
-
 
 	function getZero(value) {
 		if (value >= 0 && value < 10) {
@@ -88,4 +96,103 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	}
 	setClock('.timer', deadline);
+
+	//Modal
+	const modalBtns = document.querySelectorAll('[data-modal]'),
+		close = document.querySelector('[data-close]'),
+		modalOverlay = document.querySelector('.modal');
+
+	function showModal() {
+		modalOverlay.classList.toggle('show');
+		modalOverlay.classList.add('fade');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);// if modal was open remove timer for showing modal
+	}
+
+	
+	const modalTimerId = setTimeout(showModal, 4000);
+
+	//Modal at the end of page 
+	function showModalByScroll() {
+		//if listed height and screen height >= all height of all doc showModal
+		if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			showModal();
+
+			//removing handler after modal was showing once
+			removeEventListener('scroll', showModalByScroll);
+		}
+	}
+	
+	window.addEventListener('scroll', showModalByScroll);
+
+	
+	modalBtns.forEach(btn => {
+		btn.addEventListener('click', () => {
+			// modalOverlay.classList.add('show');
+			// modalOverlay.classList.remove('hide');
+			showModal();
+
+		});
+
+	});
+
+
+
+	function closeModal() {
+
+		// modalOverlay.classList.remove('show');
+		// modalOverlay.classList.add('hide');
+
+		modalOverlay.classList.remove('show');
+		document.body.style.overflow = ''; //?браузер сам подставит нужное свойство в пустую строку!
+	}
+
+	close.addEventListener('click', closeModal);
+
+	modalOverlay.addEventListener('click', (e) => {
+		if (e.target === modalOverlay) {
+			closeModal();
+		}
+
+	});
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code === 'Escape' && modalOverlay.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+
+
+	
+	//* modalBlock = modalOverlay.querySelector('.modal__dialog');
+	//* btns = document.querySelectorAll('button[data-modal]'),
+
+	// const closeModal = function (x) {
+	// 	x.addEventListener('click', () => {
+	// 		if (window.getComputedStyle(modalOverlay).display === 'flex') {
+	// 			modalOverlay.style.display = 'none';
+	// 		}
+	// 	});
+	// };
+
+
+	// function centerModal(mOverlay) {
+	// 	mOverlay.style.alignItems = 'center';
+	// }
+
+
+	// btns.forEach(btn => {
+	// 	btn.addEventListener('click', (e) => {
+	// 		e.preventDefault();
+	// 		if (window.getComputedStyle(modalOverlay).display === 'none') {
+	// 			centerModal(modalOverlay);
+	// 			modalOverlay.style.display = 'flex';
+	// 		}
+	// 		closeModal(close);
+
+	// 	});
+	// });
+
+
 });
